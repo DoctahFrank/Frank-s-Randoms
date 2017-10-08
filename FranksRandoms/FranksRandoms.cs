88b269bc4fb9b0ed9e601b35c3a2abbd2022bf0a
Kaleb Jubar <doctahfrank@gmail.com>
2017-10-08 13:21:59 -0400

    Committed current files

--- FranksRandoms/FranksRandoms.cs
+++ FranksRandoms/FranksRandoms.cs
@@ -1,3 +1,4 @@
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace FranksRandoms
@@ -13,5 +14,66 @@ namespace FranksRandoms
 				AutoloadSounds = true
 			};
 		}
-	}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.Silk, 15);
+            recipe.AddIngredient(ItemID.BlueandBlackDye, 1);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(ItemID.MiningShirt, 1);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.Silk, 10);
+            recipe.AddIngredient(ItemID.BlueandBlackDye, 1);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(ItemID.MiningPants, 1);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddRecipeGroup("Wood", 15);
+            recipe.AddIngredient(ItemID.SpellTome);
+            recipe.AddIngredient(ItemID.BossTrophyOgre);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(ItemID.BookStaff);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.HellstoneBar, 15);
+            recipe.AddIngredient(ItemID.Feather, 3);
+            recipe.AddIngredient(ItemID.BossTrophyOgre);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(ItemID.DD2PhoenixBow);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.HellstoneBar, 15);
+            recipe.AddRecipeGroup("IronBar", 3);
+            recipe.AddIngredient(ItemID.BossTrophyOgre);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(ItemID.DD2SquireDemonSword);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.Coral, 10);
+            recipe.AddIngredient(ItemID.Seashell, 3);
+            recipe.AddIngredient(ItemID.Starfish, 3);
+            recipe.AddIngredient(ItemID.BossTrophyOgre);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(3835);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddIngredient(ItemID.HellstoneBar, 15);
+            recipe.AddIngredient(ItemID.FallenStar, 3);
+            recipe.AddIngredient(ItemID.BossTrophyOgre);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(3836);
+            recipe.AddRecipe();
+
+            recipe = new ModRecipe(this);
+            recipe.AddRecipeGroup("", 10);
+        }
+    }
 }
---
+++ FranksRandoms/NPCs/FrankNPC.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FranksRandoms.NPCs
+{
+    public class FrankNPC : GlobalNPC
+    {
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            switch (type)
+            {
+                case NPCID.Dryad:
+                    if(NPC.downedMechBossAny)
+                    {
+                        shop.item[nextSlot].SetDefaults(ItemID.NightKey);
+                        shop.item[nextSlot].shopCustomPrice = 150000;
+                        nextSlot++;
+                        shop.item[nextSlot].SetDefaults(ItemID.LightKey);
+                        shop.item[nextSlot].shopCustomPrice = 150000;
+                        nextSlot++;
+                    }
+                    break;
+
+                case NPCID.Merchant:
+                    shop.item[nextSlot].SetDefaults(ItemID.Chest);
+                    shop.item[nextSlot].shopCustomPrice = 1000;
+                    nextSlot++;
+                    if(NPC.downedBoss1)
+                    {
+                        shop.item[nextSlot].SetDefaults(ItemID.GoldChest);
+                        shop.item[nextSlot].shopCustomPrice = 2500;
+                        nextSlot++;
+                    }
+                    break;
+
+                case NPCID.GoblinTinkerer:
+                    if (NPC.downedPlantBoss)
+                    {
+                        shop.item[nextSlot].SetDefaults(ItemID.PumpkinMoonMedallion);
+                        shop.item[nextSlot].shopCustomPrice = 1000000;
+                        nextSlot++;
+                        shop.item[nextSlot].SetDefaults(ItemID.NaughtyPresent);
+                        shop.item[nextSlot].shopCustomPrice = 1000000;
+                        nextSlot++;
+                    }
+                    break;
+            }
+        }
+    }
+}
